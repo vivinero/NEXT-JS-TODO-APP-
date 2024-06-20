ad5c8b8de7915b31c7df08945bb3c1882464f6d3
vivinero <viviannzemeke@gmail.com>
2024-06-20 17:31:50 +0100

    This a Next JS TODO App

--- src/app/page.js
+++ src/app/page.js
@@ -4,91 +4,42 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.container}>
+          <div className={styles.left}>
+              <div className={styles.welcome}>
+               <h2>WELCOME TO DEV VEE</h2>
+              </div>
+          </div>
+          <div className={styles.right}>
+              <form className={styles.form}>
+                <div className={styles.contentTop}>
+                  <h2>Sign Up</h2>
+                  <h5>Don't have an accout with DevVee? No worries.</h5>
+                </div>
+                  <div className={styles.myInput}>
+                    <input type="text" placeHolder="first name"/>
+                    <input type="text" placeHolder="last name"/>
+                  </div>
+                  <div className={styles.myInput}>
+                    <input type="text" placeHolder="email name"/>
+                    <input type="text" placeHolder="gender"/>
+                  </div>
+                  <div className={styles.myInput}>
+                    <input type="text" placeHolder="phone number"/>
+                    <input type="text" placeHolder="DOB"/>
+                  </div>
+                  <div className={styles.myInput}>
+                    <input type="text" placeHolder="Password"/>
+                    <input type="text" placeHolder="Confirm password"/>
+                  </div>
+                  <div className={styles.happyText}>
+                    <p>Happy to Join the team? Click on the Sign Up button</p>
+                  </div>
+                  <div className={styles.buttonHolder}>
+                    <button>Sign Up</button>
+                  </div>
+              </form>
+          </div>
       </div>
     </main>
   );
